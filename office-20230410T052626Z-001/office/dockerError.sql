53753efe6c9509e123f967f31fb7134eb1873a9c
chiragr12 <chiragrathi303@gmail.com>
2023-06-12 10:44:33 +0530

    error/chaincodeError.sql modify

--- office-20230410T052626Z-001/office/dockerError.sql
+++ office-20230410T052626Z-001/office/dockerError.sql
@@ -1,7 +1,6 @@
 Error1 :- permission denied in docker-compose on linux
 
 Solution:- 
-This three line command run and Docker-compose error gone.
 sudo usermod -aG docker $USER 
 sudo chgrp docker /usr/local/bin/docker-compose 
 sudo chmod 750 /usr/local/bin/docker-compose
